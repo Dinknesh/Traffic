4d8b382210d28314e5fa3a3b18cf544383a20a63
LysAMa <ladouceurlyse44@gmail.com>
2018-08-20 19:51:09 -0400

    customize my branch

--- app/src/main/java/com/example/amanda/traffic/UI/LoginActivity.java
+++ app/src/main/java/com/example/amanda/traffic/UI/LoginActivity.java
@@ -1,4 +1,4 @@
-package com.example.amanda.traffic;
+package com.example.amanda.traffic.UI;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -14,6 +14,7 @@ import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.example.amanda.traffic.R;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
